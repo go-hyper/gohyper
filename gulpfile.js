a410f501398c1bf7cf01cf4993c6fc613b4d6e67
Jenny Gebske <jennifer@gebske.de>
2015-10-06 22:44:17 +0200

    install angular with bower

--- gulpfile.js
+++ gulpfile.js
@@ -3,10 +3,9 @@
 var gulp = require('gulp');
 var zip = require('gulp-zip');
 
-var files = ['manifest.json', 'js/background.js', 'images/test.png', 'popup.html'];
+var files = ['manifest.json', 'js/background.js', 'images/test.png', 'popup.html', 'js/popup.js', 'js/gohyper.js', 'bower_components/angular/angular.js'];
 
 var xpiName = 'test.xpi';
-
 gulp.task('default', function () {
   gulp.src(files)
     .pipe(zip(xpiName))
--- js/gohyper.js
+++ js/gohyper.js
@@ -1,11 +1,12 @@
 var gohyper = angular.module('gohyper', []);
 
-gohyper.controller('ExtensionController', function() {
+gohyper.controller('ExtensionController', function ($scope) {
 
-    $scope.tags = ["Test", "Bla"];
+    $scope.tags = ["Test", "abc"];
     $scope.push = function (input) {
       $scope.tags.push(input);
       $scope.input= "";
     };
+    console.log($scope.tags);
 
   });
