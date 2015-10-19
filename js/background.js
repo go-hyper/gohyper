0943fd3eeba7140178d598d7d6499c9cd4e9546f
Jenny Gebske <jennifer@gebske.de>
2015-10-19 22:14:28 +0200

    remove typo

--- js/background.js
+++ js/background.js
@@ -3,7 +3,7 @@
 chrome.browserAction.setBadgeText({text: 'Test'});
 chrome.browserAction.setBadgeBackgroundColor({color: '#0091EA'});
 
-/*chrome.browserAction.onClicked.addListener(function(aTab) {
+/*chrome.browserAction.onClicked.addListener(function(tab) {
   chrome.tabs.query({'url': ''}, (tabs) => {
     if (tabs.length === 0) {
       // There is no tab
