8954c6d16974a2e6a94ffc1f7ebd02f8dde0c3f3
Jenny Gebske <jennifer@gebske.de>
2015-11-02 18:18:00 +0100

    add NavigationController

--- js/gohyper.js
+++ js/gohyper.js
@@ -1,17 +1,26 @@
 var gohyper = angular.module('gohyper', []);
 
-gohyper.controller('QuoteController', function ($scope) {
-  $scope.tags = [];
-  $scope.push = function (input) {
-    if ($scope.tags.indexOf(input) == -1) {
-      $scope.tags.push(input);
-    }
-    $scope.input = "";
-  };
+gohyper
+  .controller('NavigationController', function($scope, $location){
+    $scope.isActive = function(str) {
+      var reg = new RegExp('^'+str+'(\/.*)?$');
+      return reg.test($location.path());
+    };
+  });
 
-  // dummy data
-  $scope.data = [["Lorem ipsum dolor sit amet, consectetur adipisici elit, sed eiusmod tempor incidunt ut labore et dolore magna aliqua.", "http://www.tagesschau.de"],
-    ["Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquid ex ea commodi consequat.", "http://www.mi.fu-berlin.de/inf/"],
-    ["Quis aute iure reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.", "http://www.fu-berlin.de"]
-  ];
-});
+gohyper
+  .controller('QuoteController', function($scope) {
+    $scope.tags = [];
+    $scope.push = function (input) {
+      if ($scope.tags.indexOf(input) == -1) {
+        $scope.tags.push(input);
+      }
+      $scope.input = "";
+    };
+
+    // dummy data
+    $scope.data = [["Lorem ipsum dolor sit amet, consectetur adipisici elit, sed eiusmod tempor incidunt ut labore et dolore magna aliqua.", "http://www.tagesschau.de"],
+      ["Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquid ex ea commodi consequat.", "http://www.mi.fu-berlin.de/inf/"],
+      ["Quis aute iure reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.", "http://www.fu-berlin.de"]
+    ];
+  });
