a5bfca9d8d83af9f93bf21810c6a0e57bfc69df3
Jenny Gebske <jennifer@gebske.de>
2015-11-02 18:13:28 +0100

    add container for ng-view

--- js/gohyper.js
+++ js/gohyper.js
@@ -1,19 +1,17 @@
 var gohyper = angular.module('gohyper', []);
 
-gohyper.controller('ExtensionController', function ($scope) {
+gohyper.controller('QuoteController', function ($scope) {
+  $scope.tags = [];
+  $scope.push = function (input) {
+    if ($scope.tags.indexOf(input) == -1) {
+      $scope.tags.push(input);
+    }
+    $scope.input = "";
+  };
 
-    $scope.tags = [];
-    $scope.push = function (input) {
-      if ($scope.tags.indexOf(input) == -1) {
-        $scope.tags.push(input);
-      }
-      $scope.input = "";
-    };
-
-    // dummy data
-    $scope.data = [["Lorem ipsum dolor sit amet, consectetur adipisici elit, sed eiusmod tempor incidunt ut labore et dolore magna aliqua.", "http://www.tagesschau.de"],
-      ["Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquid ex ea commodi consequat.", "http://www.mi.fu-berlin.de/inf/"],
-      ["Quis aute iure reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.", "http://www.fu-berlin.de"]
-    ];
-
-  });
+  // dummy data
+  $scope.data = [["Lorem ipsum dolor sit amet, consectetur adipisici elit, sed eiusmod tempor incidunt ut labore et dolore magna aliqua.", "http://www.tagesschau.de"],
+    ["Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquid ex ea commodi consequat.", "http://www.mi.fu-berlin.de/inf/"],
+    ["Quis aute iure reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.", "http://www.fu-berlin.de"]
+  ];
+});
