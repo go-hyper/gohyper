3ff7dd7b88c9f81e117cd494d770c6d06f9d8f47
Jenny Gebske <jennifer@gebske.de>
2015-11-07 17:09:04 +0100

    change xpiName, image and version name as well as the extension's description

--- gulpfile.js
+++ gulpfile.js
@@ -4,10 +4,10 @@ var gulp = require('gulp');
 var zip = require('gulp-zip');
 
 var files = ['manifest.json', 'js/background.js', 'js/popup.js', 'js/gohyper.js', 'bower_components/angular/angular.min.js',
-  'bower_components/angular-route/angular-route.min.js', 'images/test.png', 'popup.html', 'quote.html', 'notepad.html',
+  'bower_components/angular-route/angular-route.min.js', 'images/gohyper.png', 'popup.html', 'quote.html', 'notepad.html',
   'info.html', 'css/gohyper.css'];
 
-var xpiName = 'test.xpi';
+var xpiName = 'gohyper.xpi';
 gulp.task('default', function() {
   gulp.src(files)
     .pipe(zip(xpiName))
