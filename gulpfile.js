31eea6d7948f633a1c7da8dcb79aa6b8c10f32b8
Jenny Gebske <jennifer@gebske.de>
2015-11-08 13:43:40 +0100

    add angular-indexed-db to gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -4,8 +4,8 @@ var gulp = require('gulp');
 var zip = require('gulp-zip');
 
 var files = ['manifest.json', 'js/background.js', 'js/popup.js', 'js/gohyper.js', 'bower_components/angular/angular.min.js',
-  'bower_components/angular-route/angular-route.min.js', 'images/gohyper.png', 'popup.html', 'quote.html', 'notepad.html',
-  'info.html', 'css/gohyper.css'];
+  'bower_components/angular-route/angular-route.min.js', 'bower_components/angular-indexedDB/angular-indexed-db.js',
+  'images/gohyper.png', 'popup.html', 'quote.html', 'notepad.html', 'info.html', 'css/gohyper.css'];
 
 var xpiName = 'gohyper.xpi';
 gulp.task('default', function() {
