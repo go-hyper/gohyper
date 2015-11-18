3391652871b82fd22c3785f2b163b96ffd7d6514
Jenny Gebske <jennifer@gebske.de>
2015-11-18 23:22:27 +0100

    add communication between background/event page and content script for context menu's onclick event

--- js/content.js
+++ js/content.js
@@ -1,6 +1,12 @@
-// Send a message containing the page details back to the event page
+// send a message containing the page details back to the event/background page
 chrome.runtime.sendMessage({
   'title': document.title,
   'currentUrl': window.location.href,
   'quote': window.getSelection().toString()
 });
+
+// wait for messages from event/background page belonging to context menu's onclick events
+chrome.runtime.onMessage.addListener(function(message, sender) {
+  // TODO
+  console.log(message);
+})
