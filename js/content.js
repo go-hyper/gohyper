ddc40eaa1a426582f0fe28de9e3c3c58c37b38ff
Jenny Gebske <jennifer@gebske.de>
2015-11-19 22:54:41 +0100

    add function to highlight selected text

--- js/content.js
+++ js/content.js
@@ -5,8 +5,26 @@ chrome.runtime.sendMessage({
   'quote': window.getSelection().toString()
 });
 
+// http://stackoverflow.com/questions/3223682/change-css-of-selected-text-using-javascript
+function makeEditableAndHighlight(colour) {
+  var range, sel = window.getSelection();
+  if (sel.rangeCount && sel.getRangeAt) {
+    range = sel.getRangeAt(0);
+  }
+  document.designMode = "on";
+  if (range) {
+    sel.removeAllRanges();
+    sel.addRange(range);
+  }
+  // Use HiliteColor since some browsers apply BackColor to the whole block
+  if (!document.execCommand("HiliteColor", false, colour)) {
+    document.execCommand("BackColor", false, colour);
+  }
+  document.designMode = "off";
+}
+
 // wait for messages from event/background page belonging to context menu's onclick events
 chrome.runtime.onMessage.addListener(function(message, sender) {
+  makeEditableAndHighlight("yellow");
   // TODO
-  console.log(message);
-})
+});
