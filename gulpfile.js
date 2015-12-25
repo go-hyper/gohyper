622882c46903a3b53f8fd0147604d42e19416bd2
Jenny Gebske <jennifer@gebske.de>
2015-12-25 16:51:10 +0100

    add rangy and adjust gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -6,10 +6,12 @@ var zip = require('gulp-zip');
 var files = ['manifest.json', 'js/*', 'bower_components/angular/angular.min.js',
   'bower_components/angular-route/angular-route.min.js', 'bower_components/angular-bootstrap/ui-bootstrap.min.js',
   'node_modules/dom-position-serializer/index.js', 'bower_components/font-awesome/css/font-awesome.css',
-  'bower_components/jquery/dist/jquery.min.js', 'images/*', 'html/*', 'overview.html', 'iframe.html', 'css/*'];
+  'bower_components/jquery/dist/jquery.min.js', 'bower_components/rangy/rangy-core.min.js',
+  'bower_components/rangy/rangy-classapplier.min.js', 'bower_components/rangy/rangy-highlighter.min.js',
+  'bower_components/rangy/rangy-serializer.min.js', 'images/*', 'html/*', 'overview.html', 'iframe.html', 'css/*'];
 
 gulp.task('default', function() {
-  return gulp.src(files)
+  return gulp.src(files, {base: './'})
     .pipe(zip('gohyper.zip'))
     .pipe(gulp.dest('./dist'));
 });
