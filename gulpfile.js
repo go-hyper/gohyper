dc6e7d99ec4e887a6509a62a1aa79acff7e9895a
Jenny Gebske <jennifer@gebske.de>
2015-12-25 16:35:46 +0100

    change gulpfile and use min versions

--- gulpfile.js
+++ gulpfile.js
@@ -3,14 +3,13 @@
 var gulp = require('gulp');
 var zip = require('gulp-zip');
 
-var files = ['manifest.json', 'js/background.js', 'js/gohyper.js', 'js/overview.js', 'bower_components/angular/angular.min.js',
-  'bower_components/angular-route/angular-route.min.js', 'bower_components/angular-bootstrap/ui-bootstrap.js',
-  'node_modules/dom-position-serializer/index.js', 'images/*', 'html/quote.html', 'html/notepad.html',
-  'html/quote_edit.html', 'iframe.html', 'css/gohyper.css', 'css/content.css', 'css/iframe.css'];
+var files = ['manifest.json', 'js/*', 'bower_components/angular/angular.min.js',
+  'bower_components/angular-route/angular-route.min.js', 'bower_components/angular-bootstrap/ui-bootstrap.min.js',
+  'node_modules/dom-position-serializer/index.js', 'bower_components/font-awesome/css/font-awesome.css',
+  'bower_components/jquery/dist/jquery.min.js', 'images/*', 'html/*', 'overview.html', 'iframe.html', 'css/*'];
 
-var xpiName = 'gohyper.xpi';
 gulp.task('default', function() {
-  gulp.src(files)
-    .pipe(zip(xpiName))
-    .pipe(gulp.dest('.'));
+  return gulp.src(files)
+    .pipe(zip('gohyper.zip'))
+    .pipe(gulp.dest('./dist'));
 });
